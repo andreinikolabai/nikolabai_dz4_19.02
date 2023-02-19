3decdc1451403b28bbd2fe2674a75ff6e4b71000
andreinikolabai <andreiplus2000@gmail.com>
2023-02-19 18:44:52 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+let a=Number(prompt("First number",2));
+let b=Number(prompt("Second number",2));
+c=a+b;
+d=a-b;
+e=a*b;
+f=a/b;
+
+alert(a+"+"+b+"="+c);
+alert(a+"-"+b+"="+d);
+alert(a+"*"+b+"="+e);
+alert(a+"/"+b+"="+f);
